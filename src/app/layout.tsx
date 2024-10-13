42267c0ae7fc0e7f9bab130db028ee493924f938
Eliécer Hernández <paraguanads@dexkit.com>
2024-10-13 16:50:02 -0400

    Fixes and optimizations: Added GA component, optimizations, redirects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,24 +3,38 @@ import "./globals.css";
 import { sora } from './fonts';
 import ClientLayout from './layout-client';
 import messages from './i18n/messages';
+import Analytics from './components/Analytics';
 
 export async function generateMetadata({ params }: { params: { locale: string } }): Promise<Metadata> {
   const localeMessages = messages[params.locale as keyof typeof messages] || messages.en;
 
   return {
-    title: localeMessages['layout.title'],
+    title: {
+      template: '%s | DexKit',
+      default: localeMessages['layout.title'],
+    },
     description: localeMessages['layout.description'],
     keywords: "DexKit, DApps, Web3, Blockchain, No-Code, Low-Code, Decentralized Finance, DeFi, Ethereum, Polygon, Arbitrum, NFT Marketplace",
     openGraph: {
       title: localeMessages['layout.title'],
       description: localeMessages['layout.description'],
       images: [{ url: "/imgs/dexkit_og.png" }],
+      locale: params.locale,
+      type: 'website',
     },
     twitter: {
       card: "summary_large_image",
       title: localeMessages['layout.title'],
       description: localeMessages['layout.description'],
       images: ["/imgs/dexkit_og.png"],
+    },
+    alternates: {
+      canonical: `https://dexkit.com/${params.locale}`,
+      languages: {
+        'en-US': '/en',
+        'es-ES': '/es',
+        'pt-BR': '/pt',
+      },
     },
   };
 }
@@ -36,6 +50,7 @@ export default function RootLayout({
     <html className={`${sora.variable}`} lang={params.locale}>
       <body className={sora.className}>
         <ClientLayout>{children}</ClientLayout>
+        <Analytics />
       </body>
     </html>
   );
