3b523739de2e7a5ea240eaa27be02a30c9599351
Eliécer Hernández <paraguanads@dexkit.com>
2025-08-12 11:05:50 -0300

    feat: Add security headers in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,35 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none';",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), payment=()',
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return Object.entries(redirectsJson).map(([source, { destination, permanent }]) => ({
       source,
